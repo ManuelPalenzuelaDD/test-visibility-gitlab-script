bfb9bc74809f23f4c789a79db2cae070c39e4f8a
Manuel Palenzuela Merino <manuel.palenzuela@datadoghq.com>
2024-08-08 12:46:31 +0200

    clean

--- script.sh
+++ script.sh
@@ -80,9 +80,3 @@ fi
 chmod +x ./install_test_visibility.sh
 
 DD_CIVISIBILITY_AUTO_INSTRUMENTATION_PROVIDER="gitlab" DD_CIVISIBILITY_INSTRUMENTATION_LANGUAGES="${LANGUAGES}" ./install_test_visibility.sh | while IFS= read -r line; do echo "export $line"; done
-
-# Without echoing export and sed and evaling
-#eval $(DD_SITE="datad0g.com" DD_CIVISIBILITY_INSTRUMENTATION_LANGUAGES="js" DD_API_KEY="test123" ./script.sh | sed 's/^/export /')
-
-# Echoing export and just evaling
-#eval $(DD_SITE="datad0g.com" DD_CIVISIBILITY_INSTRUMENTATION_LANGUAGES="js" DD_API_KEY="test123" ./script.sh)
